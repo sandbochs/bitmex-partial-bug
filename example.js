4fc5f44064d67e1af355359d82589cc3ba3a4da5
ko0f <ko0f@users.noreply.github.com>
2018-08-25 14:02:34 +0300

    initial commit

---
+++ example.js
@@ -0,0 +1,20 @@
+'use strict';
+
+if (process.argv.length < 4) {
+  console.error(`Not enough parameters.`);
+  console.log(`Usage: node example.js [TESTNET_API_KEY] [TESTNET_API_SECRET]`);
+}
+
+const BitMEXClient = require('./index');
+// See 'options' reference below
+const client = new BitMEXClient({testnet: true, apiKeyID: process.argv[2], apiKeySecret: process.argv[3]});
+// handle errors here. If no 'error' callback is attached. errors will crash the client.
+client.on('error', console.error);
+client.on('open', () => console.log('Connection opened.'));
+client.on('close', () => console.log('Connection closed.'));
+client.on('initialize', () => console.log('Client initialized, data is flowing.'));
+
+client.addStream('XBTUSD', 'execution', function(data, symbol, tableName) {
+  console.log(`Got update for ${tableName}:${symbol}. Current state:\n${JSON.stringify(data).slice(0, 100)}...`);
+  // Do something with the table data...
+});
